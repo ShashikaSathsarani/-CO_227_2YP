9597fe2d76e6ba0f3c47236b3f52cbd70374a3a6
ShashikaSathsarani <e21362@eng.pdn.ac.lk>
2025-08-27 08:46:00 +0530

    frontend changes

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Peravers Kiosk</h1>
+      <p>This is your basic website structure.</p>
+    </div>
+  );
+}
+
+export default App;
